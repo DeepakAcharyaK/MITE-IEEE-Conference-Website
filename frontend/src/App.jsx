d50358561d38194adfff4ba25c23048385f24591
deepakacharyak <deepakacharyak887@gmail.com>
2025-02-03 11:10:51 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./Pages/Landing";
 import Contacts from "./Pages/Contacts";
-import Layout from "./Layout";
 import RegistrationDetails from "./Pages/RegistrationDetails";
 import Track from "./Pages/Track";  
 import Committee from "./Pages/Committee";
+import Layout from "./Layout";
 
 function App() {
   return (
